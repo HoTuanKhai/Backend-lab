3d756df66a00df4cf2a8ecd50809ed87caccd2b5
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2024-10-21 05:14:16 +0700

    Modified some folder

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from "react-router-dom"
+import './Admin.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import { Routes,Route } from 'react-router-dom'
+import AddProduct from '../../Components/AddProduct/AddProduct'
+import ListProduct from '../../Components/ListProduct/ListProduct'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+const Admin = () =>{
+  return(
+    <div className='admin'>
+        <Sidebar/>
+        <Routes>
+            <Route path='/addproduct' element={<AddProduct/>}/>
+            <Route path='/listproduct' element={<ListProduct/>}/>            
+        </Routes>
+    </div>
+  )
+}
+
+export default Admin

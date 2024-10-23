467cb72f394bc93ea9178731b08f2c1b2107d834
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2024-10-24 03:45:12 +0700

    Modified and Complete display image on each category

--- src/Components/Asset/new_collections.js
+++ src/Components/Asset/new_collections.js
@@ -1,65 +1,65 @@
-import p1_img from "./product_12.jpg";
-import p2_img from "./product_35.jpg";
-import p3_img from "./product_14.jpg";
-import p4_img from "./product_8.jpg";
-import p5_img from "./product_15.jpg";
-import p6_img from "./product_2.jpg";
-import p7_img from "./product_17.jpg";
-import p8_img from "./product_28.jpg";
+import p1_img from "./product_11.jpg";
+import p2_img from "./product_12.jpg";
+import p3_img from "./product_19.jpg";
+import p4_img from "./product_22.jpg";
+import p5_img from "./product_23.jpg";
+import p6_img from "./product_24.jpg";
+import p7_img from "./product_35.jpg";
+import p8_img from "./product_36.jpg";
 
 let new_collections = [
   {
-    id: 12,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    id: 11,
+    name: "Lux Figurine League of Legends",
     image: p1_img,
     new_price: 50.0,
     old_price: 80.5,
   },
   {
-    id: 35,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    id: 12,
+    name: "Set Figurine League of Legends",
     image: p2_img,
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 14,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    id: 19,
+    name: "Luffy Gear 5 Figurine One Piece",
     image: p3_img,
     new_price: 60.0,
     old_price: 100.5,
   },
   {
-    id: 8,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    id: 22,
+    name: "Zoro Figurine One Piece",
     image: p4_img,
     new_price: 100.0,
     old_price: 150.0,
   },
   {
-    id: 15,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    id: 23,
+    name: "Boa Hancook Figurine One Piece",
     image: p5_img,
     new_price: 50.0,
     old_price: 80.5,
   },
   {
-    id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    id: 24,
+    name: "Sanji Figurine One Piece",
     image: p6_img,
     new_price: 85.0,
     old_price: 120.5,
   },
   {
-    id: 17,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    id: 35,
+    name: "Gengar Figurine Pokemon",
     image: p7_img,
     new_price: 60.0,
     old_price: 100.5,
   },
   {
-    id: 28,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    id: 36,
+    name: "Lucario Figurine Pokemon",
     image: p8_img,
     new_price: 100.0,
     old_price: 150.0,

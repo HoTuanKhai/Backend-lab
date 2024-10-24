92521e1e9fa609299d8201f5b6871016d7b82a55
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2024-10-24 22:05:26 +0700

    Complete the basic of the project

--- backend/index.js
+++ backend/index.js
@@ -224,3 +224,47 @@ app.get('/newcollections', async (req,res) => {
     console.log("NewCollection Fetched");
     res.send(newcollection);
 })
+
+// Creating middleware to fetch user
+const fetchUser = async(req,res,next) =>{
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({errors:"Please authenticate using valid token"})
+    }
+    else{
+        try{
+            const data = jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        } catch (error){
+            res.status(401).send({errors:"please authenticate using a valid token"})
+        }
+    }
+}
+
+// Creating endpoint for adding products in cart data
+app.post('/addtocart',fetchUser,async(req,res) => {
+    
+    console.log("Added",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] +=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData: userData.cartData});
+    res.send("Added")
+})
+
+// Create endpoint for remove product from cart data
+app.post('/removefromcart',fetchUser,async(req,res) => {
+    
+    console.log("Removed",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] -=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData: userData.cartData});
+    res.send("Removed")
+})
+
+// Create endpoint to get cart data
+app.post('/getcart',fetchUser,async (req,res) => {
+    console.log("GetCart");
+    let userData = await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
